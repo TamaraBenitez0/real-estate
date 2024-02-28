a26c769c6f9795b373325ac878cd4e9007ed4e86
TamaraBenitez0 <tamara16@live.com.ar>
2024-02-28 02:00:39 -0300

    project creation backend

---
+++ backend/RealEstateManagement/RealEstateManagement/Program.cs
@@ -0,0 +1,52 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var config = builder.Configuration;
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("RealStateManagement", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.EnableTryItOutByDefault());
+}
+
+app.UseHttpsRedirection();
+app.UseCors("RealStateManagement");
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
